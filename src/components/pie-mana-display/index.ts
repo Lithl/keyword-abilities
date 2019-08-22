aa79179b34cea0d1cbe91e877631eadfb884b392
Lithl <blanketyblankman@gmail.com>
2019-08-22 14:49:28 -0700

    added fallback gradient for when there are no cards with current filters

--- src/components/pie-mana-display/index.ts
+++ src/components/pie-mana-display/index.ts
@@ -21,11 +21,24 @@ export class PieManaDisplay extends PolymerElement {
   }
 
   protected computePieStyle_() {
+    const fallback = `background: conic-gradient(
+      white 0%, white 11%,
+      red 11%, red 13.5%,
+      white 13.5%, white 36%,
+      red 36%, red 38%,
+      white 38%, white 61%,
+      red 61%, red 63%,
+      white 63%, white 86%,
+      red 86%, red 88%,
+      white 88%)`;
+    if (this.total === 0) return fallback;
+
     const white = this.numW / this.total * 100;
     const blue = white + this.numU / this.total * 100;
     const black = blue + this.numB / this.total * 100;
     const red = black + this.numR / this.total * 100;
     const green = red + this.numG / this.total * 100;
+    if (green === 0 && this.numC === 0) return fallback;
     return `background: conic-gradient(
       white 0%, white ${white}%,
       blue ${white}%, blue ${blue}%,
